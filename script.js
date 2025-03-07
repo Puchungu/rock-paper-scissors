aaf43ff62fabbebb45c48f421506bd93cae87b60
Puchungu <nelsonalvarenga1000@gmail.com>
2025-03-07 11:53:02 -0600

    Se movio la funcion playround afuera del for

--- script.js
+++ script.js
@@ -16,33 +16,37 @@ function getHumanChoice(){
     return human.toLowerCase();
 };
 
+function playRound(humanChoice, computerChoice){
+    if (humanChoice == 'rock' && computerChoice == 'paper'){
+        console.log('You lose, computer wins')
+        pc += 1;
+    } else if (humanChoice == 'paper' && computerChoice == 'rock'){
+        console.log('You win, computer sucks.')
+        human += 1;
+    } else if (humanChoice == 'scissors' && computerChoice == 'rock'){
+        console.log('You lose, computer wins')
+        pc += 1;
+    } else if (humanChoice == 'rock' && computerChoice == 'scissors'){
+        console.log('You win, computer sucks')
+        human += 1;
+    } else if (humanChoice == 'paper' && computerChoice == 'scissors'){
+        console.log('You lose, computer wins')
+        pc += 1;
+    } else if (humanChoice == 'scissors' && computerChoice == 'paper'){
+        console.log('You win, computer sucks')
+        human += 1;
+    } else {
+        console.log('Its a tie')
+    } 
+};
+
+
+
+
 for (let i = 0; i < 5; i++){
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
     console.log('HUMAN ELIGIO: ' + humanSelection + ' PC choose: ' + computerSelection);
-    function playRound(humanChoice, computerChoice){
-        if (humanChoice == 'rock' && computerChoice == 'paper'){
-            console.log('You lose, computer wins')
-            pc += 1;
-        } else if (humanChoice == 'paper' && computerChoice == 'rock'){
-            console.log('You win, computer sucks.')
-            human += 1;
-        } else if (humanChoice == 'scissors' && computerChoice == 'rock'){
-            console.log('You lose, computer wins')
-            pc += 1;
-        } else if (humanChoice == 'rock' && computerChoice == 'scissors'){
-            console.log('You win, computer sucks')
-            human += 1;
-        } else if (humanChoice == 'paper' && computerChoice == 'scissors'){
-            console.log('You lose, computer wins')
-            pc += 1;
-        } else if (humanChoice == 'scissors' && computerChoice == 'paper'){
-            console.log('You win, computer sucks')
-            human += 1;
-        } else {
-            console.log('Its a tie')
-        } 
-    };
     playRound(humanSelection, computerSelection);
 }
 
